7eb44c5d0c3d235e2b4937699f3e819a4b2b8114
Salah baddou <sed.baddou@gmail.com>
2019-11-06 16:38:00 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
- var fs = require('fs');
- 
-fs.readFile('DATA', 'utf8', function(err, contents) {
-    console.log(contents);
-});
- 
-console.log('after calling readFile');
+const fs = require('fs'),
+      path = require('path');
+
+function readFileHelper(p) {     // #2
+  p = path.resolve(p);           // #3
+  fs.readFile(p,                 // #4
+    'utf8', (err, data) => {
+    if (err) throw err;
+    console.log(data);
+  });
+}
+
+readFileHelper(process.argv[2]); // #1
